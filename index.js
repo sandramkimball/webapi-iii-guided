cad2905e8e6903ad7d14a91f3cc676fb61106449
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-10-30 10:29:22 -0700

    gateKeeper password mod

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Your host:4000 is a little clingy. *\n');
+server.listen(8000, () => {
+  console.log('\n* Your host:8000 is a bit odd. *\n');
 });
--- server.js
+++ server.js
@@ -1,28 +1,46 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan'); //for logging(?)
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 //tres amigos
+function logger(req, res, next){
+  console.log(`My Logging Func:[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next();
+}
+
 function dateLogger(req, res, next){
   console.log(new Date().toISOString());
 
   next();
 }
 
-function visitedLogger(req, res, next){
-  console.log(`GET/${}`);
+function gateKeeper(req, res, next){
+  const password = req.headers.password || '';
+  //NEW WAY TO READ CLIENT'S DATA!!!
+  //data comes in body, url params, query string, headers
+  //Password not put/post (for body only) but in header(get) because it's more secure.
 
-  next();
+  if(password === null){
+    res.status(400).json({message: 'Password required to enter.'})
+  } else if (password.toLowerCase() === 'melon'){
+    next()
+  } else {
+    res.status().json({you: 'Gate still locked.'})
+  }
 }
 
 //global middleware
+server.use(gateKeeper);//custome
 server.use(helmet());//third party
 server.use(express.json());//built in
+server.use(logger)//custom middleware
 server.use(dateLogger)//custom middleware
-server.use(visitedLogger)//custom middleware
+server.use(morgan('dev'));//third party
 
 server.use('/api/hubs', hubsRouter);
 
@@ -30,8 +48,8 @@ server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
   res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Cici's Hubs API</p>
+    <h2>Carnival of Hubs API</h2>
+    <p>Welcome, ${nameInsert}, to the FreakShow</p>
     `);
 });
 
